22274f5565a9091be6c866da3507e7d20cfd27df
shubham110190 <shubham.pv90@gmail.com>
2021-03-27 00:46:33 +0530

    Added REadline

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace OOPsConceptConsoleAppVS2019
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
