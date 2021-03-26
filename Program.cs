e12df7a60c64d51ed7e1569eafa5ea0a047ef7a9
shubham110190 <shubham.pv90@gmail.com>
2021-03-27 00:50:49 +0530

    Added new TestClass1

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace OOPsConceptConsoleAppVS2019
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            TestClass1 obj = new TestClass1();
             Console.ReadLine();
         }
     }
---
+++ TestClass1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPsConceptConsoleAppVS2019
+{
+   public class TestClass1
+    {
+        public TestClass1()
+        {
+            Console.WriteLine("Test Class1");
+        }
+    }
+}
